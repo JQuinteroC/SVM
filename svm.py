06cdafb9856fe7424e09f53318d826ab2ac370cf
JQuinteroC <jlqc1772@gmail.com>
2020-03-03 21:09:32 -0500

    Se agrego una predicción usando el modelo SVM

--- svm.py
+++ svm.py
@@ -3,13 +3,26 @@
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv("lenses.csv")
 
-x_train, x_test, y_train, y_test = train_test_ split(data.loc[::,['Edad', 'Lentes', 'Astigmatismo', 'Lagrimeo']], data['Tipo'])
+x_train, x_test, y_train, y_test = train_test_split(data.loc[::,['Edad', 'Lentes', 'Astigmatismo', 'Lagrimeo']], data['Tipo'])
 
-clf = SVC()
+clf = SVC(gamma = 'scale')
 
 clf.fit(x_train, y_train)
 
+print("precision media del modelo")
 print(clf.score(x_test, y_test))
+
+print("para una personaje joven, miope, astigmatica y con lagrimeo normal, tendra que usar unos lentes: ")
+res = clf.predict(np.reshape(np.array([1,1,1,2]), (1,4)))
+if res == 1:
+    print("Lentes de contacto duras")
+elif res == 2:
+    print("Lentes de contacto blandas")
+elif res == 3:
+    print("No debe usar lentes de contacto")
+else:
+    print("No se puede determinar")
